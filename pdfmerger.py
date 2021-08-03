d24d576708f438f0c56d995149a3a3192292f442
Challa Koti Reddy <kotireddychalla2002@gmail.com>
2021-08-03 21:14:12 +0530

    Added the functionality to specify file name in argument list

--- pdfmerger.py
+++ pdfmerger.py
@@ -1,9 +1,18 @@
+import sys
 import PyPDF2
 
-def merge(*args):
+def merge(file_list, output_file):
     merged_pdf = PyPDF2.PdfFileMerger()
-    for file in args:
+    for file in file_list:
         with open(file, 'rb') as f:
             pdf_reader = PyPDF2.PdfFileReader(f)
             merged_pdf.append(pdf_reader)
-    merged_pdf.write('mergedPdf.pdf')
+    merged_pdf.write(output_file)
+
+def main():
+    file_list = sys.argv[1:-1]
+    output_file = sys.argv[-1]
+    merge(file_list, output_file)
+
+if __name__ == "__main__":
+    main()
